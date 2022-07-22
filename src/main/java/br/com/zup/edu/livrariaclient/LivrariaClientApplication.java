da5e9171dac3573228754e7ac0302ee0d21aaae7
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-07-22 16:24:21 -0300

    Commit inicial

---
+++ src/main/java/br/com/zup/edu/livrariaclient/LivrariaClientApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.livrariaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariaClientApplication.class, args);
+	}
+
+}
